7cb2cba4b4a061a1f9389e5b20db698ef5cab29d
Kent Davidson <kent@marketacumen.com>
2016-02-19 20:57:42 -0500

    Use blobs so db doesn't corrupt unicode PHP strings

--- classes/class/server/data.sql
+++ classes/class/server/data.sql
@@ -6,6 +6,6 @@ CREATE TABLE `{table}` (
 	`id` integer unsigned not null PRIMARY KEY AUTO_INCREMENT,
 	`server` integer unsigned not null,
 	`name` varchar(64) not null,
-	`value` longtext not null,
+	`value` longblob not null,
 	unique `sname` (`server`, `name`)
 );
