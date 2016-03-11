ea1974d2135f07cb58bece6e2b2d223b8be3a5dc
Kent Davidson <kent@marketacumen.com>
2016-03-11 08:49:37 -0500

    Language Country two-letter change, add locale/dialect support

--- modules/world/classes/class/language.sql
+++ modules/world/classes/class/language.sql
@@ -1,10 +1,10 @@
 -- Database: mysql
 CREATE TABLE `{table}` (
 	`id` int(11) unsigned not null auto_increment,
-	`code` char(2) not null default '',
-	`dialect` integer unsigned null,
+	`code` char(2) not null,
+	`dialect` char(2) null,
 	`name` varchar(64) null,
 	PRIMARY KEY  (`id`),
-	UNIQUE KEY `code` (`code`),
+	UNIQUE KEY `code` (`code`,`dialect`),
 	KEY `dialect` (`dialect`)
 );
