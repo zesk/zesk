a59a3030a2362f10fb7bc63f00a0026e2e296569
Kent Davidson <kent@marketacumen.com>
2017-06-14 14:41:19 -0400

    misnamed git path

---
+++ classes/Class/Server/Data.sql
@@ -0,0 +1,11 @@
+-- Database: mysql
+-- COLUMN: Server -> server
+-- COLUMN: Name -> name
+-- COLUMN: Value -> value
+CREATE TABLE `{table}` (
+	`id` integer unsigned not null PRIMARY KEY AUTO_INCREMENT,
+	`server` integer unsigned not null,
+	`name` varchar(64) not null,
+	`value` longblob not null,
+	unique `sname` (`server`, `name`)
+);
