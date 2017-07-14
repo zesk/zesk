343bef2eee840fa9790db1e513bd3c20e9fb89ab
Kent Davidson <kent@marketacumen.com>
2017-07-14 17:31:51 -0400

    fixing travis ci

--- bin/test-zesk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export APPLICATION_ROOT="$(cd $(dirname "$BASH_SOURCE")/..; pwd)"
-cd $APPLICATION_ROOT
-composer update
-./bin/zesk --Database::names::default=mysqli://root:@127.0.0.1/test_zesk test --debug --verbose --sandbox
