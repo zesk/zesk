a6e073295d79e7f2584fa71301f32e3e4bccdefd
Kent Davidson <kent@marketacumen.com>
2017-07-17 17:34:57 -0400

    Allow zesk.sh to interpret --cd in command line before selecting zesk-command.php to run

--- bin/zesk.sh
+++ bin/zesk.sh
@@ -9,7 +9,22 @@ if [ -z "$php" ]; then
 	echo "PHP is not found in PATH: " . $PATH 1>&2
 	exit 1
 fi
+handle_cd() {
+	local name
+	while [ $# -gt 2 ]; do
+		name=$1
+		shift
+		# TODO: Does this have security implications? e.g. cd passing args later in argument list where they may normally be ignored, for example
+		if [ "$name" = "--cd" ]; then
+			# echo "Changing directory to $1" 1>&2
+			cd $1
+			return 0
+		fi
+	done
+	return 0
+}
 start=`pwd`
+handle_cd "$@"
 app_root=
 while [ -z "$app_root" ]; do
 	current=`pwd`
@@ -35,6 +50,7 @@ elif [ ! -d "$app_root/vendor/bin/" ]; then
 fi
 for binary in "$app_root/bin/zesk-command.php" "$app_root/vendor/bin/zesk-command.php"; do
 	if [ -x "$binary" ]; then
+		echo "$php" "$binary" "$@"
 		exec "$php" "$binary" "$@"
 	fi
 done
