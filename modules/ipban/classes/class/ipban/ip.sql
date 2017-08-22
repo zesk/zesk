6e7b7102311dc590c8ba890c6772501fe33cf6a6
Kent Davidson <kent@zesk.com>
2017-08-22 02:16:05 -0400

    removing ipban to its own module

--- modules/ipban/classes/class/ipban/ip.sql
+++
@@ -1,7 +0,0 @@
--- Database: mysql
-CREATE TABLE `{table}` (
-	`ip`		integer unsigned NOT NULL PRIMARY KEY,
-	`when`		timestamp NOT NULL DEFAULT 0,
-	`status`	tinyint DEFAULT 0,
-	INDEX `status` (`status`)
-);
--- modules/ipban/classes/class/ipban/tag/type.sql
+++
@@ -1,8 +0,0 @@
--- Database: mysql
--- COLUMN: ID -> id
--- COLUMN: Name -> name
-CREATE TABLE `{table}` (
-	`id`	integer unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT ,
-	`name`	varchar(64) NOT NULL,
-	UNIQUE `name` (`name`)
-);
