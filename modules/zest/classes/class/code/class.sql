c8b8ddde10470756e4961282d0499e1b3801dc87
Kent Davidson <kent@marketacumen.com>
2017-08-23 02:08:04 -0400

    moving to own repo

--- modules/zest/classes/class/code/class.sql
+++
@@ -1,8 +0,0 @@
--- Database: SQLite3
-CREATE TABLE {table} (
-	id				integer unsigned NOT NULL PRIMARY KEY,
-	name			varchar(128) NOT NULL DEFAULT '',
-	source			integer unsigned NOT NULL,
-);
-
-CREATE UNIQUE INDEX {table}_name ON {table} (name);
