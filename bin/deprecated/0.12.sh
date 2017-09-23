9e22efa6e40a9b436896943cb20dc30112f56200
Kent Davidson <kent@marketacumen.com>
2017-09-23 12:15:30 -0400

    deprecated additionas

--- bin/deprecated/0.12.sh
+++ bin/deprecated/0.12.sh
@@ -12,8 +12,16 @@ pause() {
 # echo 'Function `zesk\Kernel::sort_weight_array[_reverse]` has been renamed to `zesk_sort_weight_array[_reverse]`'
 # php-find.sh sort_weight_array | grep -v 'zesk_sort_weight_array'
 # pause
-#
-# echo 'Function `zesk\Process_Tools::process_code[_...]` now takes `$application` as the first parameter'
-# php-find.sh Process_Tools::process_code | grep -v 'app'
-# pause
-#
+
+echo 'Function `Currency::from_code` now takes `$application` as the first parameter'
+php-find.sh Currency::from_code | grep -v 'app'
+pause
+
+echo 'Function `System::ip_addresses` now takes `$application` as the first parameter'
+php-find.sh System::ip_addresses | grep -v 'app'
+pause
+
+echo 'Function `Job::instance` now takes `$application` as the first parameter'
+php-find.sh Job::instance | grep -v 'app'
+pause
+
