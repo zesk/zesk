40e1e3cf41b247675de83ab11c177097ebf3f2a5
Kent Davidson <kent@marketacumen.com>
2017-10-31 10:34:45 -0400

    Fixing `zesk help` command

--- command/help.php
+++ command/help.php
@@ -17,7 +17,7 @@ class Command_Help extends Command_Base {
 	protected $option_help = array(
 		'no-core' => 'Skip all Zesk core commands'
 	);
-
+	
 	/**
 	 *
 	 * @var array
@@ -38,7 +38,7 @@ class Command_Help extends Command_Base {
 		$this->save_aliases($this->aliases);
 		return 0;
 	}
-
+	
 	/**
 	 */
 	function collect_command_files() {
@@ -99,7 +99,7 @@ class Command_Help extends Command_Base {
 				continue;
 			}
 			$this->verbose_log("Registering $class");
-			$this->zesk->classes->register($class);
+			$this->application->classes->register($class);
 		}
 	}
 	function process_class($class) {
@@ -154,17 +154,17 @@ class Command_Help extends Command_Base {
 	}
 	function collect_help() {
 		$command_files = $this->collect_command_files();
-
+		
 		$this->load_commands($command_files);
-
+		
 		$this->aliases = array();
 		$this->categories = array();
-
+		
 		$subclasses = $this->application->classes->subclasses("zesk\Command");
 		foreach ($subclasses as $subclass) {
 			$this->process_class($subclass);
 		}
-
+		
 		ksort($this->categories);
 	}
 	function save_aliases(array $aliases) {
