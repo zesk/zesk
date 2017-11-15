1226e32b6a4247422b6252a3b5e63f9a8ab8442d
Kent Davidson <kent@zesk.com>
2017-11-15 09:22:57 -0500

    Added `zesk\Kernel::copyright_holder()` to retrieve Zesk copyright holder

---
+++ modules/test/classes/Test/Example.php
@@ -0,0 +1,29 @@
+<?php
+
+// This file serves as a template for creating new tests and should only be updated to reflect new test to be generated.
+// All line comments will be stripped from this file before being copied into new tests.
+// Also, the Example_Test value is replaced with the test name
+// Some comments are magical, such as the use identifier below which is replaced with an optional use clause for
+// an optional parent class
+//
+/**
+ *
+ * @author {author}
+ * @package {package}
+ * @subpackage {subpackage}
+ * @copyright &copy; {year} {copyright}
+ */
+namespace zesk;
+
+// use
+
+/**
+ *
+ * @author {author}
+ *
+ */
+class Example_Test extends PHPUnit_TestCase {
+	// Tests go here
+}
+
+// This is a comment. I love comments.
