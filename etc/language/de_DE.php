e0c18525c79d94e7248e38f76db674803ee494f8
Kent Davidson <kent@marketacumen.com>
2018-01-19 09:16:24 -0500

    better extension

---
+++ etc/language/de_DE.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * $URL: https://code.marketacumen.com/zesk/trunk/etc/language/de_DE.inc $
+ * @package zesk
+ * @subpackage locale
+ * @author kent
+ * @copyright Copyright &copy; 2008, Market Acumen, Inc.
+ * Created on Thu Jan 22 12:10:40 EST 2009 12:10:40
+ */
+return array();
+
+
---
+++ etc/language/es.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * $URL: https://code.marketacumen.com/zesk/trunk/etc/language/es.inc $
+ * @package zesk
+ * @subpackage locale
+ * @author kent
+ * @copyright Copyright &copy; 2008, Market Acumen, Inc.
+ * Created on Thu Jan 22 12:10:40 EST 2009 12:10:40
+ */
+$tt = array();
+
+$tt['and'] = "y";
+$tt['or'] = "o";
+$tt['name'] = "nombre";
+$tt['special'] = "nombre";
+$tt['current'] = "current";
+$tt['organic'] = 'org&aacute;nico';
+
+$tt['Date:=January'] = 'enero';
+$tt['Date:=February'] = 'febrero';
+$tt['Date:=March'] = 'marzo';
+$tt['Date:=April'] = 'abril';
+$tt['Date:=May'] = 'mayo';
+$tt['Date:=June'] = 'junio';
+$tt['Date:=July'] = 'julio';
+$tt['Date:=August'] = 'agosto';
+$tt['Date:=September'] = 'septiembre';
+$tt['Date:=October'] = 'octubre';
+$tt['Date:=November'] = 'noviembre';
+$tt['Date:=December'] = 'diciembre';
+
+$tt['Date-short:=Jan'] = 'enero';
+$tt['Date-short:=Feb'] = 'feb.';
+$tt['Date-short:=Mar'] = 'marzo';
+$tt['Date-short:=Apr'] = 'abr.';
+$tt['Date-short:=May'] = 'mayo';
+$tt['Date-short:=Jun'] = 'jun.';
+$tt['Date-short:=Jul'] = 'jul.';
+$tt['Date-short:=Aug'] = 'agosto';
+$tt['Date-short:=Sep'] = 'sept.';
+$tt['Date-short:=Oct'] = 'oct.';
+$tt['Date-short:=Nov'] = 'nov.';
+$tt['Date-short:=Dec'] = 'dic.';
+
+$tt['Date:=Sunday'] = 'domingo';
+$tt['Date:=Monday'] = 'lunes';
+$tt['Date:=Tuesday'] = 'martes';
+$tt['Date:=Wednesday'] = 'mi&eacute;rcoles';
+$tt['Date:=Thursday'] = 'jueves';
+$tt['Date:=Friday'] = 'viernes';
+$tt['Date:=Saturday'] = 's&aacute;bado';
+
+$tt['Date-short:=Sun'] = 'dom.';
+$tt['Date-short:=Mon'] = 'lun.';
+$tt['Date-short:=Tue'] = 'mar.';
+$tt['Date-short:=Wed'] = 'mi&eacute;.';
+$tt['Date-short:=Thu'] = 'jue.';
+$tt['Date-short:=Fri'] = 'vie.';
+$tt['Date-short:=Sat'] = 'sab.';
+
+$tt['Time:=pm'] = 'Time:=pm';
+$tt['Time:=PM'] = 'Time:=PM';
+
+$tt['today'] = 'hoy';
+$tt['yesterday'] = 'ayer';
+$tt['this week'] = 'este semana';
+$tt['last week'] = 'semana';
+$tt['this month'] = 'este mes';
+$tt['last month'] = 'mes pasado';
+$tt['this year'] = 'este a&ntilde;o';
+$tt['last year'] = 'el a&ntilde;o pasado';
+
+$tt['microsecond'] = 'microsegundo';
+$tt['millisecond'] = 'milisegundo';
+$tt['second'] = 'segundo';
+$tt['minute'] = 'minuto';
+$tt['hour'] = 'hora';
+$tt['day'] = 'd&iacute;a';
+$tt['days'] = 'd&iacute;as';
+$tt['month'] = 'mes';
+$tt['months'] = 'meses';
+$tt['week'] = 'semana';
+$tt['weeks'] = 'semanas';
+$tt['year'] = 'a&ntilde;o';
+$tt['years'] = 'a&ntilde;os';
+$tt['zesk\\Locale::now_string:=in {duration}'] = 'en {duration}';
+$tt['zesk\\Locale::now_string:={duration} ago'] = 'hace {duration}';
+$tt['yes'] = 'si';
+$tt['no'] = 'non';
+
+$tt['Control_Pager:=Showing {0} - {1} of {2}'] = 'Mostrando {0} - {1} de {2}';
+$tt['Control_Pager:=Show'] = 'Mostrar';
+
+$tt['zesk\\Locale::plural_word:=no {word}'] = 'hay {word}';
+$tt['zesk\\Locale::plural_word:=one {word}'] = '1 {word}';
+$tt['zesk\\Locale::plural_word:={number} {word}'] = '{number} {word}';
+
+$tt['login'] = 'iniciar sesi&oacute;n';
+$tt['password'] = 'contrase&ntilde;a';
+$tt['sign up'] = 'alistarse';
+
+return $tt;
+
