91c7350325ef8d893bb758d39561ec58fbbc4a5b
Kent Davidson <kent@marketacumen.com>
2018-02-02 14:21:17 -0500

    `zesk\Response` refactoring

--- modules/chosen/classes/Module/Chosen.php
+++ modules/chosen/classes/Module/Chosen.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * 
+ *
  */
 namespace zesk;
 
 /**
- * 
+ *
  * @author kent
  *
  */
@@ -23,27 +23,32 @@ class Module_Chosen extends Module_JSLib {
 		"include_group_label_in_selected" => true
 	);
 	protected $jquery_ready = array();
-	
+
 	//"\$('.chosen-select').chosen(zesk.get_path('modules.chosen'));"
 	protected static $jquery_ready_pattern = array(
 		"{selector}.chosen(\$.extend(zesk.get_path('modules.chosen'),{json_options}));"
 	);
 	public function initialize() {
 		parent::initialize();
-		$this->javascript_settings_inherit['no_results_text'] = __('No results match');
-		$classes = $this->option_list("hook_classes", "zesk\\Control_Select");
+		$locale = $this->application->locale;
+		$this->javascript_settings_inherit['no_results_text'] = $locale->__('No results match');
+		$classes = $this->option_list("hook_classes", Control_Select::class);
 		$hooks = $this->application->hooks;
+		$chosen = $this;
+		$ready_pattern = self::$jquery_ready_pattern;
 		foreach ($classes as $class) {
-			$chosen = $this;
-			$hooks->add("$class::initialized", function (Control_Select $widget) use ($chosen) {
+			$hooks->add("$class::render", function (Widget $widget) use ($chosen, $ready_pattern) {
 				if ($widget->option_bool("skip-chosen") || $widget->is_single()) {
 					return;
 				}
 				$widget->add_class("chosen-select");
-				$widget->response()->jquery(map(self::$jquery_ready_pattern, array(
+				$code = map($ready_pattern, array(
 					"selector" => $widget->jquery_target_expression(),
 					"json_options" => ArrayTools::kunprefix($widget->option(ArrayTools::kprefix($chosen->javascript_settings(), "chosen_")), "chosen_")
-				)));
+				));
+				$widget->response()
+					->html()
+					->jquery($code);
 			});
 		}
 	}
