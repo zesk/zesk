f0babd9ecb714c8fb0db3d6eae786a2f1e8fffa6
Kent Davidson <kent@marketacumen.com>
2018-02-05 08:16:18 -0500

    fixing Forgot controller

--- modules/forgot/classes/Control/Forgot.php
+++ modules/forgot/classes/Control/Forgot.php
@@ -10,31 +10,31 @@
 namespace zesk;
 
 /**
- * 
+ *
  * @author kent
  *
  */
 class Control_Forgot extends Control_Edit {
 	/**
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $class = "zesk\\Forgot"; // TODO fix to __NAMESPACE__ when TODO-PHP7 only
-	
+
 	/**
 	 * Header theme
 	 *
 	 * @var string
 	 */
 	protected $theme_prefix = null; //"zesk/control/forgot/prefix";
-	
+
 	/**
 	 * Header theme
 	 *
 	 * @var string
 	 */
 	protected $theme_header = null; //"zesk/control/forgot/header";
-	
+
 	/**
 	 * Footer theme
 	 *
@@ -47,9 +47,9 @@ class Control_Forgot extends Control_Edit {
 	 * @var string
 	 */
 	protected $theme_suffix = null; //"zesk/control/forgot/suffix";
-	
+
 	/**
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $options = array(
@@ -60,36 +60,36 @@ class Control_Forgot extends Control_Edit {
 	 * @var Forgot
 	 */
 	protected $object = null;
-	
+
 	/**
 	 *
 	 * @var User
 	 */
 	private $auth_user = null;
-	
+
 	/**
-	 * 
+	 *
 	 * @return \zesk\Widget[]|boolean[]
 	 */
 	function hook_widgets() {
 		$this->form_name("forgot_form");
-		
+
 		$ww = array();
-		
+
 		$ww[] = $w = $this->widget_factory($this->option("widget_login", 'zesk\\Control_Text'))->names('login', __($this->option("label_login", "Login")));
 		$w->required(true);
 		$w->default_value($this->request->get('login'));
-		
+
 		$ww[] = $w = $this->widget_factory('zesk\\Control_Password')->names('login_password', $this->option("label_password", __("New Password")));
 		$w->set_option('encrypted_column', 'new_password');
 		$w->set_option('confirm', true);
 		$w->required(true);
-		
+
 		$ww[] = $w = $this->widget_factory('zesk\\Control_Button')
 			->names('forgot', $this->option("label_button", __("Send me an email")))
 			->add_class('btn-primary btn-block')
 			->nolabel(true);
-		
+
 		return $ww;
 	}
 	public function submitted() {
@@ -100,7 +100,7 @@ class Control_Forgot extends Control_Edit {
 			return false;
 		}
 		/* @var $user User */
-		$this->auth_user = $this->orm_factory("zesk\\User")->login($this->object->login)->find();
+		$this->auth_user = $this->application->orm_factory("zesk\\User")->login($this->object->login)->find();
 		$this->auth_user = $this->call_hook_arguments("find_user", array(
 			$this->auth_user
 		), $this->auth_user);
@@ -117,9 +117,9 @@ class Control_Forgot extends Control_Edit {
 			$object->session = $session = $this->session();
 			$object->code = md5(microtime() . mt_rand(0, mt_getrandmax()));
 			$object->store();
-			
+
 			$object->notify($this->request);
-			
+
 			$session->forgot = $object->id();
 			$this->response->redirect('/forgot/sent');
 		} else {
--- modules/forgot/classes/Controller/Forgot.php
+++ modules/forgot/classes/Controller/Forgot.php
@@ -5,59 +5,57 @@
 namespace zesk;
 
 /**
- * 
+ *
  * @author kent
  *
  */
 class Controller_Forgot extends Controller_Theme {
 	/**
-	 * 
+	 *
 	 */
 	function action_index() {
-		$this->control($this->widget_factory(Control_Forgot::class));
+		return $this->control($this->widget_factory(Control_Forgot::class));
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function action_unknown() {
-		$this->template->content = $this->application->theme('forgot/unknown');
+		return $this->application->theme('forgot/unknown');
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	function action_sent() {
-		$this->template->content = $this->application->theme('forgot/sent');
+		return $this->application->theme('forgot/sent');
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param unknown $hash
 	 */
 	function action_validate($hash) {
 		if (!preg_match('/[0-9a-f]{32}/i', $hash)) {
-			$this->template->content = $this->application->theme('forgot/not-found', array(
+			return $this->application->theme('forgot/not-found', array(
 				"message_type" => "invalid-token",
 				"message" => "Validation key is not valid."
 			));
-			return;
 		}
 		/* @var $forgot Forgot */
-		$forgot = $this->orm_factory(Forgot::class, array(
+		$forgot = $this->application->orm_factory(Forgot::class, array(
 			"code" => $hash
 		))->find();
 		if (!$forgot) {
-			$this->template->content = $this->application->theme('forgot/not-found', array(
+			return $this->application->theme('forgot/not-found', array(
 				"message_type" => "not-found",
 				"message" => __("Unable to find the validation token.")
 			));
-			return;
 		}
 		/* @var $user User */
 		$user = $forgot->user;
 		if (!$user instanceof User) {
-			$this->template->content = $forgot->theme('forgot/not-found', array(
+			return $forgot->theme('forgot/not-found', array(
 				"message_type" => "user-not-found",
 				"message" => "User not found."
 			));
@@ -65,16 +63,14 @@ class Controller_Forgot extends Controller_Theme {
 		}
 		$session = $this->application->session();
 		if ($forgot->id() !== $session->forgot) {
-			$this->template->content = $forgot->theme('same-browser');
-			return;
+			return $forgot->theme('same-browser');
 		}
 		if (!$forgot->member_is_empty('updated')) {
-			$this->template->content = $forgot->theme("already");
-			return;
+			return $forgot->theme("already");
 		}
-		
+
 		$forgot->validated();
-		
-		$this->template->content = $forgot->theme("success");
+
+		return $forgot->theme("success");
 	}
 }
