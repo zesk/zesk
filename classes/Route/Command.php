d34cf63d7f205b4f98e6ad6c3a1f5b8e192616bc
Kent Davidson <kent@marketacumen.com>
2018-02-27 20:21:09 -0500

    Removing `$zesk` global

--- classes/Route/Command.php
+++ classes/Route/Command.php
@@ -2,25 +2,23 @@
 namespace zesk;
 
 /**
- * 
+ *
  * @author kent
  *
  */
 class Route_Command extends Route {
-	protected function _execute() {
-		global $zesk;
-		$app = $this->router->application;
-		/* @var $locale \zesk\Locale */
-		$response = $app->response;
+	protected function _execute(Response $response) {
+		$app = $response->application;
+
 		$debug = $this->option_bool('debug');
-		
+
 		$command = avalue($this->original_options, "command");
 		$args = $this->option_array("arguments", array());
-		
+
 		$command = map($command, $this->named);
 		$args = map($args, $this->args + $this->named);
 		if ($debug) {
-			$zesk->logger->debug("{class}: executing: command={command}, args={args}", array(
+			$app->logger->debug("{class}: executing: command={command}, args={args}", array(
 				"command" => $command,
 				"class" => get_class($this),
 				"args" => $args
@@ -28,20 +26,19 @@ class Route_Command extends Route {
 		}
 		$theme_arguments = $this->option_array("theme arguments", array());
 		try {
-			$result = $zesk->process->execute_arguments($command, $args);
+			$result = $app->process->execute_arguments($command, $args);
 			if ($debug) {
-				$zesk->logger->debug("{class}: Result is {result}", array(
+				$app->logger->debug("{class}: Result is {result}", array(
 					"class" => get_class($this),
 					"result" => $result
 				));
 			}
-			$content = theme($this->option("theme", "route/command"), array(
+			$content = $app->theme($this->option("theme", "route/command"), array(
 				"content" => $result
 			) + $theme_arguments);
 		} catch (Exception $e) {
-			global $zesk;
-			$zesk->hooks->call("exception", $e);
-			$content = theme($this->option("theme", "route/command/empty"), array(
+			$app->hooks->call("exception", $e);
+			$content = $app->theme($this->option("theme", "route/command/empty"), array(
 				"content" => $this->option("empty", ""),
 				"empty" => true
 			) + $theme_arguments);
