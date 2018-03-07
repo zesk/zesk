6a5171f51433d36d6dbb9634a7b3a9b75e0aae51
Kent Davidson <kent@marketacumen.com>
2018-03-07 18:31:08 -0500

    JSONJS updated to PSR4, etc

---
+++ modules/jsonjs/classes/Module.php
@@ -0,0 +1,10 @@
+<?php
+namespace zesk\JSONJS;
+
+class Module extends \zesk\Module_JSLib {
+	protected $javascript_paths = array(
+		"/share/jsonjs/json2.js" => array(
+			'share' => true
+		)
+	);
+}
