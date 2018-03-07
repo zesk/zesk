bcd46a9b4f73d9d0d126bd742ff66c1ef132ca89
Kent Davidson <kent@marketacumen.com>
2018-03-07 17:31:32 -0500

    `zesk\Module_ThreeJS` is now `zesk\ThreeJS\Module` and PSR-4

--- modules/threejs/classes/Module.php
+++ modules/threejs/classes/Module.php
@@ -1,8 +1,23 @@
 <?php
-class Module_ThreeJS extends zesk\Module {
-	public static function head(zesk\Request $request, zesk\Response $response) {
-		$response->javascript("/share/threejs/three.js", array(
+/**
+ * @package zesk-modules
+ * @subpackage ThreeJS
+ * @author kent
+ * @copyright &copy; 2018 Market Acumen, Inc.
+ */
+namespace zesk;
+
+/**
+ * @author kent
+ */
+namespace zesk\ThreeJS;
+
+use zesk\Module_JSLib;
+
+class Module extends Module_JSLib {
+	protected $javascript_paths = array(
+		"/share/threejs/three.js" => array(
 			"share" => true
-		));
-	}
+		)
+	);
 }
