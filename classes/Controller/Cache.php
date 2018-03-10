790ccb2100d44c738f0045711c1a7e924ee90609
Kent Davidson <kent@marketacumen.com>
2018-03-10 16:43:27 -0500

    Updating usage of `zesk\Response` API

--- classes/Controller/Cache.php
+++ classes/Controller/Cache.php
@@ -1,6 +1,9 @@
 <?php
 /**
- *
+ * @package zesk
+ * @subpackage Controller
+ * @author kent
+ * @copyright &copy; 2018 Market Acumen, Inc.
  */
 namespace zesk;
 
@@ -10,6 +13,15 @@ namespace zesk;
  *
  */
 class Controller_Cache extends Controller {
+
+	/**
+	 * Convert a request for file stored elsewhere and store it where the web server will then serve it thereafter.
+	 *
+	 * Useful for populating a file system from alternate sources such as share paths, or from the database or a remote store, for example.
+	 *
+	 * @param string $contents
+	 * @return NULL|\zesk\Response Returns NULL if a problem occurred, or zesk\Response to serve contents as file.
+	 */
 	protected function request_to_file($contents) {
 		$file = $this->request->path();
 		if (!File::path_check($file)) {
@@ -25,20 +37,17 @@ class Controller_Cache extends Controller {
 		$docroot = $this->application->document_root();
 		$cache_file = Directory::undot(path($docroot, $file));
 		if (!begins($cache_file, $docroot)) {
-			$this->application->hooks->call("security", "User cache file \"{cache_file}\" does not match doc root \"{docroot}\"", array(
+			$this->application->hooks->call("security", "User cache file \"{cache_file}\" does not match document root \"{docroot}\"", array(
 				"cache_file" => $cache_file,
 				"docroot" => $docroot
 			));
 			return null;
 		}
-		if ($this->request->get('nocache') === '123123') {
-			$this->response->content_type = MIME::from_filename($cache_file);
-			$this->response->header('Content-Length', strlen($contents));
-			$this->response->content = $contents;
-			return;
+		if ($this->request->get('nocache') === $this->option("nocache_key", microtime(true))) {
+			return $this->response->content_type(MIME::from_filename($cache_file))->header('Content-Length', strlen($contents))->content($contents);
 		}
 		Directory::depend(dirname($cache_file), $this->option("cache_directory_mode", 0775));
 		file_put_contents($cache_file, $contents);
-		$this->response->file($cache_file);
+		return $this->response->file($cache_file);
 	}
 }
