1ec1ea489e44df25e5b80d43732e0d9a77f32a50
Kent Davidson <kent@zesk.com>
2018-05-13 20:49:03 -0400

    fixing zesk app when composer not run

--- zesk.application.php
+++ zesk.application.php
@@ -11,6 +11,9 @@ namespace zesk;
 if (!isset($GLOBALS['__composer_autoload_files'])) {
 	if (is_file(__DIR__ . '/vendor/autoload.php')) {
 		require_once __DIR__ . '/vendor/autoload.php';
+	} else {
+		fprintf(STDERR, "Missing vendor directory\n");
+		exit(1);
 	}
 } else {
 	require_once __DIR__ . '/autoload.php';
