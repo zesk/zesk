4efd425d1f15c03603f12b241d418e87b49a5580
Kent Davidson <kent@marketacumen.com>
2018-05-13 20:34:00 -0400

    Adding `$context` to `zesk\Exception_File_NotFound` to give ... context to exception

--- classes/Exception/File/NotFound.php
+++ classes/Exception/File/NotFound.php
@@ -1,17 +1,19 @@
 <?php
 
 /**
- * 
+ *
  */
 namespace zesk;
 
 /**
- * 
+ *
  * @author kent
  *
  */
 class Exception_File_NotFound extends Exception_FileSystem {
-	function __construct($filename = null) {
-		parent::__construct($filename, "{filename} not found");
+	function __construct($filename = null, $context = null) {
+		parent::__construct($filename, "{filename} not found {context}", array(
+			"context" => $context
+		));
 	}
 }
