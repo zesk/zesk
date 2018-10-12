cf0a07d1ed2de123cbe028d1d0dd17b151bea2b9
Kent Davidson <kent@marketacumen.com>
2018-10-12 09:47:05 -0400

    Fixed an issue with `zesk schema` which would output the application hooks in a rather verbose manner

--- modules/orm/command/schema.php
+++ modules/orm/command/schema.php
@@ -24,19 +24,19 @@ class Command_Schema extends Command_Base {
 		"update" => "boolean",
 		"*" => "string"
 	);
-	
+
 	/**
 	 *
 	 * @var array
 	 */
 	protected $register_classes = array();
-	
+
 	/**
 	 *
 	 * @var array
 	 */
 	protected $results = array();
-	
+
 	/**
 	 *
 	 * @return \zesk\multitype:
@@ -44,13 +44,13 @@ class Command_Schema extends Command_Base {
 	public function results() {
 		return $this->results;
 	}
-	
+
 	/**
 	 */
 	protected function synchronize_before() {
 		self::_synchronize_suffix("update");
 	}
-	
+
 	/**
 	 */
 	protected function synchronize_after() {
@@ -85,19 +85,19 @@ class Command_Schema extends Command_Base {
 			$this,
 			"hook_callback"
 		);
-		
+
 		$app = $this->application;
-		
+
 		$hook_type = "zesk\ORM::schema_$suffix";
 		$all_hooks = $this->application->hooks->find_all($hook_type);
-		
+
 		$app->logger->notice("Running all $suffix hooks {hooks}", array(
 			"hooks" => ($all = implode(", ", array_values($all_hooks))) ? $all : "- no hooks found"
 		));
 		$this->application->hooks->all_call_arguments($hook_type, array(
 			$this->application
 		), null, $hook_callback);
-		
+
 		$hook_type = "schema_$suffix";
 		$all = $app->modules->all_hook_list($hook_type);
 		$hooks_strings = array();
@@ -113,16 +113,23 @@ class Command_Schema extends Command_Base {
 		$app->modules->all_hook_arguments($hook_type, array(
 			$this->application
 		), null, $hook_callback);
-		
+
 		$app_hooks = $app->hook_list($hook_type);
+		$app_hooks_strings = "- no hooks found";
+		if (count($app_hooks) !== 0) {
+			$app_hooks_strings = array();
+			foreach ($app_hooks as $hook) {
+				$app_hooks_strings[] = $app->hooks->callable_string($hook);
+			}
+		}
 		$app->logger->notice("Running application $suffix hooks {hooks}", array(
-			"hooks" => $app_hooks ? $app_hooks : "- no hooks found"
+			"hooks" => $app_hooks_strings
 		));
 		$app->call_hook_arguments($hook_type, array(
 			$this->application
 		), null, $hook_callback);
 	}
-	
+
 	/**
 	 *
 	 * {@inheritDoc}
@@ -132,7 +139,7 @@ class Command_Schema extends Command_Base {
 		parent::initialize();
 		$this->application->register_class("zesk\ORM_Schema_File");
 	}
-	
+
 	/**
 	 *
 	 * {@inheritdoc}
@@ -141,7 +148,7 @@ class Command_Schema extends Command_Base {
 	 */
 	protected function run() {
 		$application = $this->application;
-		
+
 		if ($this->option_bool("debug")) {
 			ORM_Schema::$debug = true;
 		}
@@ -159,9 +166,9 @@ class Command_Schema extends Command_Base {
 			$classes = $this->arguments_remaining(true);
 			$this->verbose_log("Running on classes {classes}", compact("classes"));
 		}
-		
+
 		$this->synchronize_before();
-		
+
 		$database = $application->database_registry($url);
 		$this->results = $results = $application->orm_module()->schema_synchronize($database, $classes, array(
 			"check" => $this->option_bool('check')
