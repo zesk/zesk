3a040c287ef014bb77f5b0e8f2f6cdd2b96367f7
Kent Davidson <kent@marketacumen.com>
2018-10-27 16:36:09 -0400

    adding daemon bouncer utility

--- bin/daemon-bouncer.sh
+++ bin/daemon-bouncer.sh
@@ -5,13 +5,26 @@ usage() {
 	local e
 	e=$1
 	shift
-	echo $* 1>&2
-	echo $ME file command ... 1>&2
+	if [ ! -z "$*" ]; then
+		echo ERROR: $* 1>&2
+		echo 1>&2
+	fi
+	echo "Usage: $ME file command ..." 1>&2
 	echo 1>&2
-	echo file - When this file appears, run command, then delete the file 1>&2
-	echo command - Command to run with optional arguments 1>&2
+	echo "Command runs and checks file every 1 second for 60 seconds, then exits. Intended to be run as a cron task every minute." 1>&2
 	echo 1>&2
-	exit $1
+	echo "file    - When this file appears, run command, then delete the file." 1>&2
+	echo "          Directory of file must exist." 1>&2
+	echo "command - Command to run with optional arguments." 1>&2
+	echo 1>&2
+	echo "Exit codes:" 1>&2
+	echo 1>&2
+	echo "10 - file not supplied" 1>&2
+	echo "11 - command not supplied" 1>&2
+	echo "12 - file directory does not exist" 1>&2
+	echo "13 - command binary is not executable" 1>&2
+	echo "\$? - command exit code is returned if it fails" 1>&2
+	exit $e
 }
 
 CHANGED_FILE=$1
@@ -20,11 +33,17 @@ shift
 COMMAND=$1
 shift
 
+if [ -z "$CHANGED_FILE" ]; then
+	usage 10 "file not supplied" 
+fi
+if [ -z "$COMMAND" ]; then
+	usage 11 "command not supplied" 
+fi
 if [ ! -d "$CHANGED_DIR" ]; then
-	usage 10 "CHANGED_FILE ($CHANGED_FILE) directory does not exist"
+	usage 12 "CHANGED_FILE ($CHANGED_FILE) directory does not exist"
 fi
 if [ ! -x "$COMMAND" ]; then
-	usage 11 "$COMMAND must be executable" 
+	usage 13 "$COMMAND must be executable" 
 fi
 
 exec 3>&2 			# 3 is now a copy of 2
@@ -40,7 +59,7 @@ for i in {0..59}; do
 	if $COMMAND $*; then
 		rm "$CHANGED_FILE"
 	else
-		exit 100
+		exit $?
 	fi
 done
 exec 3>&- 			# close 3
