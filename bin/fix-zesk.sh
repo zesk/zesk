3d748840db5a60ab16981cdf77761aec51dda4bd
Kent Davidson <kent@marketacumen.com>
2018-11-15 18:16:11 -0500

    adding php-cs integration

--- bin/fix-zesk.sh
+++ bin/fix-zesk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export APPLICATION_ROOT="$(cd $(dirname "$BASH_SOURCE")/..; pwd)"
 
+PATH=$APPLICATION_ROOT/vendor/bin:$PATH
+
 php_fixer=php-cs-fixer
 fixer=$(which $php_fixer)
 if [ ! -x "$fixer" ]; then
