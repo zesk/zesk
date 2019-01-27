b334af03449083113ed44b56a31da098eb1960e9
Kent Davidson <kent@marketacumen.com>
2019-01-27 18:34:56 -0500

    permission-list exports correct for classes

--- modules/permission/command/permission/list.php
+++ modules/permission/command/permission/list.php
@@ -10,11 +10,25 @@ namespace zesk;
  */
 class Command_Permission_List extends Command_Base {
 	public $option_types = array(
-		'format' => 'string',
+		'format' => 'string'
 	);
-
 	public function run() {
 		$perms = $this->application->modules->object("permission")->permissions();
+		foreach ($perms['class'] as $class => $actions) {
+			foreach ($actions as $codename => $permission) {
+				assert($permission instanceof Permission);
+				apath_set($perms, array(
+					"class",
+					$class,
+					$codename
+				), $permission->members(array(
+					"id",
+					"title",
+					"class",
+					"options"
+				)));
+			}
+		}
 		$this->render_format($perms);
 		return 0;
 	}
