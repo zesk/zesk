75ec84e289a09c91a66ecde46411505eb1aafb7c
Kent Davidson <kent@marketacumen.com>
2019-02-26 13:47:19 -0500

    **Testing** `test-zesk.sh`: Set up `$APPLICATION_ROOT` and use absolute paths for binaries

--- bin/test-zesk.sh
+++ bin/test-zesk.sh
@@ -2,6 +2,9 @@
 #
 # Run Zesk test interactively while debugging and fixing things
 #
+export APPLICATION_ROOT="$(cd $(dirname "$BASH_SOURCE")/..; pwd)"
+PATH=$APPLICATION_ROOT/vendor/bin:$PATH
+
 conf=/etc/zesk.conf
 N_SECONDS=2
 opts=
@@ -12,7 +15,7 @@ opts="$opts --interactive"
 #opts="$opts --debug-command"
 go=1
 [ -f $conf ] && source $conf
-while ! ./bin/zesk --zesk___Command_Test__travis test $opts $*; do
+while ! $APPLICATION_ROOT/bin/zesk --zesk___Command_Test__travis test $opts $*; do
 	echo "========================================================="
 	echo "Running tests again after $N_SECONDS seconds ..."
 	echo "========================================================="
