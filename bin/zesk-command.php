35aedf41a1ddb1272f9215458a8fe68de8308344
Kent Davidson <kent@marketacumen.com>
2019-03-06 12:57:14 -0500

    `zesk-command.php` now adds an ini setting `display_startup_errors=Off` to prevent errors from creeping into shell outputs in `zesk\WebApp\Type\Zesk` on Mac OS X.

--- bin/zesk-command.php
+++ bin/zesk-command.php
@@ -1,7 +1,11 @@
-#!/usr/bin/env php
+#!/usr/bin/env php -d display_startup_errors=off
 <?php
 /**
  * Very important: Do not call any Zesk calls until after application laods.
+ *
+ * 2019-03-04 - Added "display_startup_errors=off" above as when zesk invoked via zesk\WebApp\Type_Zesk - it would use
+ * an alternate PHP version and display a ton of shared library errors in the version (Mac OS X/ZendServer). So adding the above
+ * hid those errors from output. Probably not a bad practice. -KMD
  *
  * @package zesk
  * @subpackage bin
