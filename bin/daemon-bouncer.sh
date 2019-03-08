fb9c650a674ea21a9b1a87396c996a80cee99768
Kent Davidson <kent@marketacumen.com>
2019-03-08 11:16:58 -0500

    **Zesk Kernel**: `daemon-bouncer.sh` renamed to `file-trigger.sh` which reflects better what it does. Old binary available for 6 months.

--- bin/daemon-bouncer.sh
+++ bin/daemon-bouncer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo $(basename "$0") is deprecated. Use file-trigger.sh
+echo $(basename "$0") is deprecated 2019-03. Use file-trigger.sh instead.
 export HERE="$(cd $(dirname "$BASH_SOURCE"); pwd)"
 $HERE/file-trigger.sh $*
