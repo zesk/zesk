32c67e5b614fe5b76dcccc49b6108ef2aaad31e3
Kent Davidson <kent.davidson@starcompliance.com>
2022-05-20 02:19:55 -0400

    wip and quote single fixes

--- modules/database/classes/Database/Table.php
+++ modules/database/classes/Database/Table.php
@@ -446,7 +446,7 @@ class Database_Table extends Hookable {
 		}
 		$after_column = $dbCol->option('after_column');
 		if ($after_column) {
-			$this->columns = ArrayTools::insert($this->columns, $after_column, [$column => $dbCol,]);
+			$this->columns = ArrayTools::insert($this->columns, $after_column, [$column => $dbCol, ]);
 		} else {
 			$this->columns[$column] = $dbCol;
 		}
