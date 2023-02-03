0ea814333c0d81392633f0fde3a7dea12fbb1c13
Kent Davidson <kent@marketacumen.com>
2023-02-03 08:47:23 -0500

    wip

--- bin/pipeline-setup.sh
+++ bin/pipeline-setup.sh
@@ -15,11 +15,6 @@ set -eo pipefail
 
 envFile="$top/.env"
 
-php=$(which php)
-if [ -z "$php" ]; then
-  echo "No PHP found in $PATH" 1>&2
-  exit $ERR_ENV
-fi
 if [ -z "$docker" ]; then
   echo "No docker found in $PATH" 1>&2
   exit $ERR_ENV
@@ -27,29 +22,25 @@ fi
 if [ -z "$dc" ]; then
   pip install docker-compose
 fi
-if test "$INSTALL_COMPOSER" || [ -z "$composer" ]; then
-  composer="$top/.bin/composer"
-  if [ ! -x "$composer" ]; then
-    [ -d "$top/.bin/" ] || mkdir -p "$top/.bin/"
-    cd "$top/.bin" || exit "$ERR_ENV"
-    "$php" "$top/docker/bin/composer-installer.php"
-    if [ ! -f composer.phar ]; then
-      echo "Composer installer failed" 1>&2
-      exit $ERR_ENV
-    fi
-    mv "$top/.bin/composer.phar" "$composer"
-    chmod +x "$composer"
-  fi
-fi
+#if test "$INSTALL_COMPOSER" || [ -z "$composer" ]; then
+#  composer="$top/.bin/composer"
+#  if [ ! -x "$composer" ]; then
+#    [ -d "$top/.bin/" ] || mkdir -p "$top/.bin/"
+#    cd "$top/.bin" || exit "$ERR_ENV"
+#    "$php" "$top/docker/bin/composer-installer.php"
+#    if [ ! -f composer.phar ]; then
+#      echo "Composer installer failed" 1>&2
+#      exit $ERR_ENV
+#    fi
+#    mv "$top/.bin/composer.phar" "$composer"
+#    chmod +x "$composer"
+#  fi
+#fi
 cd "$top" || exit "$ERR_ENV"
-echo "Installing base"
-./docker/sbin/docker-apt-base.sh > /dev/null
-echo "Installing php"
-./docker/sbin/docker-php.sh > /dev/null
-cd "$top" || exit $ERR_ENV
-if ! "$composer" install -q; then
-  echo "Composer install failed" 1>&2
-  exit "$ERR_BUILD"
-fi
+#if ! "$composer" install -q; then
+#  echo "Composer install failed" 1>&2
+#  exit "$ERR_BUILD"
+#fi
+docker run
 $dc build --no-cache --pull
 env > "$envFile"
