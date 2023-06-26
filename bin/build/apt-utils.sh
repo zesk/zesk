bfae796612a1ad44190aab1dfa4bd31624da3f92
Kent Davidson <kent@marketacumen.com>
2023-06-26 01:34:09 -0400

    python3

--- bin/build/apt-utils.sh
+++ bin/build/apt-utils.sh
@@ -14,7 +14,7 @@ me=$(basename "$0")
 top="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." || exit $err_env; pwd)"
 quietLog="$top/.build/$me.log"
 markerFile="$top/.build/.$me.marker"
-packages=(apt-utils figlet python pip)
+packages=(apt-utils figlet python3 pip3)
 apt=$(which apt-get)
 set -eo pipefail
 . "$top/bin/build/colors.sh"
