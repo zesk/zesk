a8ae66da7f4e518084ef1d5bf6c3739f558a61c3
Kent Davidson <kent@marketacumen.com>
2023-06-26 20:21:53 -0400

    release updates

--- bin/pipeline-release.sh
+++ bin/pipeline-release.sh
@@ -103,22 +103,8 @@ releaseNotesGenerate > "$changeLog"
 
 reportTiming "$start" OK
 echo
-consoleMagenta
 figlet "zesk $currentVersion"
-consoleReset
 echo
-
-#
-#========================================================================
-#
-consoleInfo "Pushing changes to GitHub ..."
-consoleDecoration "$(echoBar)"
-start=$(beginTiming)
-git commit -m "$me automatic update of CHANGELOG.md" "$changeLog"
-git push origin
-git push github
-consoleDecoration "$(echoBar)"
-reportTiming "$start" OK
 
 #
 #========================================================================
@@ -133,12 +119,13 @@ reportTiming "$start" OK
 consoleGreen "Tagging $currentVersion and pushing ... "
 consoleDecoration "$(echoBar)"
 start=$(beginTiming)
+git commit -m "$me automatic update of CHANGELOG.md" "$changeLog" || :
 git tag -d "$currentVersion" 2> /dev/null || :
 git push origin ":$currentVersion" 2> /dev/null || :
 git push github ":$currentVersion" 2> /dev/null || :
 git tag "$currentVersion"
-git push origin --tags
-git push github --tags
+git push origin --all
+git push github --all
 consoleDecoration "$(echoBar)"
 reportTiming "$start" OK
 #
